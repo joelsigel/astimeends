9f39c68e7608da3a8d8072ef7ba4d1b2a2409f16
“Joel <joelsigel@gmail.com>
2019-02-20 08:17:54 -0700

    initial

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>As Time Ends</title>
+</head>
+<meta name=viewport content="width=device-width, initial-scale=0.3">
+<link rel="stylesheet" type="text/css" href="style.css">
+<body>
+  <div class="welcome">
+    <div class="overlay"></div>
+    <div class="terminal">
+          01000001 01110011 00100000 01110100 01101001 01101101 01100101 00100000 01100101 01101110 01100100 01110011
+    </div>
+  </div>
+
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="script.js" type="text/javascript"></script>
+</html>
